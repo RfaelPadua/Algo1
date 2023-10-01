35115a9237b5687da2120925d8b387b823d438a0
Rafael de Padua Oliveira <rafael.padua@proton.me>
2023-09-30 22:13:33 -0300

    .

---
+++ Período_1/Lista 9/Python/L9_Q1.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if i % 2 == 0:
+        print(i, end = " ")
+
+print()
+
+for i in range(0,100, 2):
+    print(i, end = " ")
+
---
+++ Período_1/Lista 9/Python/L9_Q3.py
@@ -0,0 +1,8 @@
+valor1 = int(input("Informe um valor inteiro: "))
+valor2 = int(input("Informe um valor inteiro: "))
+cont = 0
+
+for i in range(0, valor2):
+    cont += valor1
+
+print(cont)
