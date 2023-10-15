02ad122a841c5702cdc851ed46ce097fc53aac69
Rafael de Padua Oliveira <rafael.padua@proton.me>
2023-10-15 20:37:58 -0300

    Update Exercícios-Funções_Structs_Vetores.c

--- 2_Período/5. Exercícios sobre Funções, Structs e Vetores/C/Exercícios-Funções_Structs_Vetores.c
+++ 2_Período/5. Exercícios sobre Funções, Structs e Vetores/C/Exercícios-Funções_Structs_Vetores.c
@@ -14,7 +14,8 @@ int main(){
 
     int menu = 666;
     int cadastroID = 10;
-    int i;
+    int i, j;
+    float soma;
 
     while(menu != 0){
         system("cls");
@@ -22,21 +23,23 @@ int main(){
         printf("Escolha uma das opcoes\n");
         printf("1. Cadastro de registro.\n");
         printf("2. Exibicao de registros.\n");
+        printf("3. Soma de todos os produtos.\n");
+        printf("4. Soma de todos os produtos baseado na descricao.\n");
         
         printf("0. Sair.\n");
         scanf("%d", &menu);fflush(stdin);
         // struct Produto produto[MAX_PESSOAS];
         struct Produto produto[10] = {
-        {"Marca 1", "Descrição 1", 5, 10.5},
-        {"Marca 2", "Descrição 2", 7, 15.2},
-        {"Marca 3", "Descrição 3", 3, 8.9},
-        {"Marca 4", "Descrição 4", 2, 12.6},
-        {"Marca 5", "Descrição 5", 9, 11.0},
-        {"Marca 6", "Descrição 6", 4, 9.8},
-        {"Marca 7", "Descrição 7", 6, 14.5},
-        {"Marca 8", "Descrição 8", 1, 7.2},
-        {"Marca 9", "Descrição 9", 8, 13.3},
-        {"Marca 10", "Descrição 10", 10, 16.7}
+        {"Marca 1", "Descricao 1", 5, 10.5},
+        {"Marca 2", "Descricao 2", 7, 15.2},
+        {"Marca 3", "Descricao 3", 3, 8.9},
+        {"Marca 4", "Descricao 4", 2, 12.6},
+        {"Marca 5", "Descricao 5", 9, 11.0},
+        {"Marca 6", "Descricao 6", 4, 9.8},
+        {"Marca 7", "Descricao 7", 6, 14.5},
+        {"Marca 8", "Descricao 8", 1, 7.2},
+        {"Marca 9", "Descricao 9", 8, 13.3},
+        {"Marca 10", "Descricao 10", 10, 16.7}
     };
 
         switch(menu){
@@ -77,6 +80,40 @@ int main(){
                     printf("\nValor: %.2f", produto[i].valor);
                     printf("\n");
                 }
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                soma = 0;
+                for(i = 0; i < cadastroID; i++){
+                    soma += produto[i].valor;
+                }
+                printf("A soma dos valores dos produtos eh %.2f.\n", soma);
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                soma = 0;
+                char consultaDescricao[50];
+
+                printf("Informe a descricao a ser consultada: ");
+                fgets(consultaDescricao, 50, stdin);
+                consultaDescricao[strlen(consultaDescricao) - 1] = '\0';
+
+                for(i = 0; i < cadastroID; i++){
+                    if(strlen(consultaDescricao) != strlen(produto[i].descricao)){
+                        continue;
+                    }
+                    for(j = 0; j < strlen(consultaDescricao); j++){
+                        if(consultaDescricao[j] != produto[i].descricao[j]){
+                            break;
+                        }
+                        if(j == strlen(consultaDescricao)){
+                            soma+=produto[i].valor;
+                        }
+                    }
+                }
+                printf("%d", soma);
                 system("pause");
                 break;
             default:
