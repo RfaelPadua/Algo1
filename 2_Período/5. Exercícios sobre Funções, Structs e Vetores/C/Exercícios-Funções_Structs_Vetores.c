1c5c2593863404d406e542502e87ff41017ea241
Rafael de Padua Oliveira <rafael.padua@proton.me>
2023-10-17 20:57:16 -0300

    Update Exercícios-Funções_Structs_Vetores.c

--- 2_Período/5. Exercícios sobre Funções, Structs e Vetores/C/Exercícios-Funções_Structs_Vetores.c
+++ 2_Período/5. Exercícios sobre Funções, Structs e Vetores/C/Exercícios-Funções_Structs_Vetores.c
@@ -13,28 +13,11 @@ struct Produto {
 
 void somaTotal(struct Produto produto[], int n);
 void somaPorDescricao(struct Produto produto[], int n, char *descricao);
+void menorValor(struct Produto produto[], int n);
+void alterar(struct Produto produto[], struct Produto novoProduto, int n, int cadastroID);
+void excluir(struct Produto produto[], int n, int *cadastroID);
 
-
-int main(){
-
-    int menu = 666;
-    int cadastroID = 10;
-    int i, j;
-    float soma;
-
-    while(menu != 0){
-        system("cls");
-
-        printf("Escolha uma das opcoes\n");
-        printf("1. Cadastro de registro.\n");
-        printf("2. Exibicao de registros.\n");
-        printf("3. Soma de todos os produtos.\n");
-        printf("4. Soma de todos os produtos baseado na descricao.\n");
-        
-        printf("0. Sair.\n");
-        scanf("%d", &menu);fflush(stdin);
-        // struct Produto produto[MAX_PESSOAS];
-        struct Produto produto[10] = {
+struct Produto produto[MAX_PESSOAS] = {
         {"Marca 1", "Descricao 1", 5, 10.5},
         {"Marca 2", "Descricao 2", 7, 15.2},
         {"Marca 3", "Descricao 3", 3, 8.9},
@@ -47,22 +30,46 @@ int main(){
         {"Marca 10", "Descricao 10", 10, 16.7}
     };
 
+int main(){
+
+    int menu = 666;
+    int cadastroID = 9;
+    int i, j;
+    float soma;
+
+    while(menu != 0){
+        system("cls");
+
+        printf("Escolha uma das opcoes\n");
+        printf("1. Cadastro de registro.\n");
+        printf("2. Exibicao de registros.\n");
+        printf("3. Soma de todos os produtos.\n");
+        printf("4. Soma de todos os produtos baseado na descricao.\n");
+        printf("5. Menor Valor.\n");
+        printf("6. Alterar.\n");
+        printf("7. Excluir.\n");
+        
+        printf("0. Sair.\n");
+        scanf("%d", &menu);fflush(stdin);
+        // struct Produto produto[MAX_PESSOAS];
+        
+
         switch(menu){
             case 1:
                 
                 char marca[100];
                 char descricao[50];
-
+                cadastroID++;
                 system("cls");
 
                 printf("Informe a marca do produto: \n");
-                fgets(marca, 100, stdin);
+                fgets(marca, 100, stdin);fflush(stdin);
                 marca[strlen(marca) - 1] = '\0';
                 strcpy(produto[cadastroID].marca, marca);
                 system("cls");
 
                 printf("Informe a descricao: \n");
-                fgets(descricao, 100, stdin);
+                fgets(descricao, 50, stdin);fflush(stdin);
                 descricao[strlen(descricao) - 1] = '\0';
                 strcpy(produto[cadastroID].descricao, descricao);
                 system("cls");
@@ -74,11 +81,11 @@ int main(){
                 printf("Informe o valor: \n");
                 scanf("%f", &produto[cadastroID].valor);
                 
-                cadastroID++;
+                
                 system("pause");
                 break;
             case 2:
-                for(i = 0; i < cadastroID; i++){
+                for(i = 0; i <= cadastroID; i++){
                     printf("\nMarca: %s", produto[i].marca);
                     printf("\nDescricao: %s", produto[i].descricao);
                     printf("\nQuantidade: %d", produto[i].quantidade);
@@ -96,7 +103,7 @@ int main(){
                 system("cls");
                 char consultaDescricao[50];
                 printf("Informe a descricao a ser consultada: ");
-                fgets(consultaDescricao, 50, stdin);
+                fgets(consultaDescricao, 50, stdin);fflush(stdin);
                 consultaDescricao[strlen(consultaDescricao) - 1] = '\0';
 
                 somaPorDescricao(produto, cadastroID, consultaDescricao);
@@ -104,8 +111,55 @@ int main(){
                 system("pause");
                 break;
             case 5:
+                system("cls");
+                menorValor(produto, cadastroID);
+                system("pause");
+                break;
+            case 6:
+                system("cls");
+                fflush(stdin);
+
+                struct Produto novoProduto;
+                int alterarID;
+                char alterarMarca[100], alterarDescricao[50];
+
+                printf("Qual registro quer alterar? (de 0 ate %d)", cadastroID);
+                scanf("%d", &alterarID);fflush(stdin);
+
+                printf("Informe a marca do produto: \n");
+                fgets(alterarMarca, 100, stdin);fflush(stdin);
+                alterarMarca[strlen(alterarMarca) - 1] = '\0';
+                strcpy(novoProduto.marca, alterarMarca);
+                system("cls");
+
+                printf("Informe a descricao: \n");
+                fgets(alterarDescricao, 50, stdin);fflush(stdin);
+                alterarDescricao[strlen(alterarDescricao) - 1] = '\0';
+                strcpy(novoProduto.descricao, alterarDescricao);
+                system("cls");
+
+                printf("Informe a quantidade: \n");
+                scanf("%d", &novoProduto.quantidade);
+                system("cls");
+
+                printf("Informe o valor: \n");
+                scanf("%f", &novoProduto.valor);
+                system("cls");
+
+                alterar(produto, novoProduto, alterarID, cadastroID);
+
                 
-            break;
+                system("pause");
+                break;
+            case 7:
+                system("cls");
+                int excluirID;
+                printf("Qual registro quer excluir? (de 0 ate %d)", cadastroID);
+                scanf("%d", &excluirID);fflush(stdin);
+
+                excluir(produto, excluirID, &cadastroID);
+                system("pause");
+                break;
             default:
                 menu = 0;
                 break;
@@ -131,5 +185,49 @@ void somaPorDescricao(struct Produto produto[], int n, char *descricao){
             soma += produto[i].valor * produto[i].quantidade;
         }
     }
-    printf("ss %.2f\n", soma);
+    printf("A soma dos produtod com essa descricao eh R$%.2f\n", soma);
+}
+
+void menorValor(struct Produto produto[], int n){
+    float min = produto[0].valor;
+    int i;
+    for(i = 0; i < n; i++){
+        if(produto[i].valor< min){
+            min = produto[i].valor;
+        }
+    }
+
+    printf("O valor do produto mais barato eh R$%.2f.\n", min);
+}
+
+void alterar(struct Produto produto[], struct Produto novoProduto, int n, int cadastroID){
+    if(n >= 0 && n <= cadastroID){
+    strcpy(produto[n].marca, novoProduto.marca);
+    strcpy(produto[n].descricao, novoProduto.descricao);
+    produto[n].quantidade = novoProduto.quantidade;
+    produto[n].valor = novoProduto.valor;
+    printf("Alteração feita com sucesso.\n");
+    }else{
+        printf("Indice invalido.\n");
+    }
+}
+
+void excluir(struct Produto produto[], int n, int *cadastroID){
+    if(n >= 0 && n < *cadastroID){
+        int i;
+        for(i = n; i < *cadastroID; i++){
+            strcpy(produto[i].marca, produto[i+1].marca);
+            strcpy(produto[i].descricao, produto[i+1].descricao);
+            produto[i].quantidade = produto[i+1].quantidade;
+            produto[i].valor = produto[i+1].valor;
+        }
+        (*cadastroID)--;
+        printf("%d\n", *cadastroID);
+        printf("Exclusao feita com sucesso.\n");
+    }else if(n == *cadastroID){
+        (*cadastroID)--;
+        printf("Exclusao feita com sucesso.\n");
+    }else{
+        printf("Indice invalido.\n");
+    }
 }
