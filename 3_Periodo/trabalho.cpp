f368e9037dec49968cb7477e00d7f32fef7cb1ce
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2024-09-05 11:40:23 +0000

    c

--- 3_Periodo/trabalho.cpp
+++ 3_Periodo/trabalho.cpp
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXTAM 5
 
-typedef int TipoApontadorEstatica;
 
 // Estrutura do Item
 typedef struct {
@@ -14,137 +12,202 @@ typedef struct {
 }TipoItem;
 
 
-// Estrutura da Fila Estatica
-typedef struct {
-    TipoItem item[MAXTAM];
-    TipoApontadorEstatica frente, tras;
-}TipoFilaEstatica;
-
-// Funções de manipulação da Fila Estatica
-void FFEVazia(TipoFilaEstatica *fila){ 
-    fila->frente = 1;
-    fila->tras = fila->frente;
-};
-
-void EnfileiraEstatica(TipoItem x, TipoFilaEstatica *fila){
-    if(fila->tras % MAXTAM + 1 == fila->frente){
-        printf("Erro fila esta cheia \n");
-    }else{
-        fila->item[fila->tras-1] = x;
-        fila->tras = fila->tras % MAXTAM + 1;
-    }
-};
-
-void DesenfileiraEstatica(TipoFilaEstatica *fila, TipoItem *item){
-    if(fila->frente == fila->tras){
-        printf("Erro fila esta vazia \n");
-    }else{
-        *item = fila->item[fila->frente - 1];
-        fila->frente = fila->frente % MAXTAM + 1;
-    }
-};
-
-void ExibeEstatica(TipoFilaEstatica fila){
-    int i;
-    for(i = fila.frente; i != fila.tras; i = i % MAXTAM + 1){
-        printf("i==%d | Placa==%s | Deslocamento==%d | Prioridade==%d\n", i, fila.item[i-1].placa, fila.item[i-1].deslocamento, fila.item[i-1].prioridade);
-    }
-};
-
-
 // Estrutura da Fila Dinamica
-typedef TipoCelula *TipoApontadorDinamica;
+typedef struct TipoCelula *TipoApontador;
 
 typedef struct TipoCelula {
 	TipoItem Item;
-	TipoApontadorDinamica Prox;
-} TipoCelula; 
+	TipoApontador Prox;
+} TipoCelula;
 
-typedef struct TipoFilaDinamica{
-    TipoApontadorDinamica Frente, Tras;
-}TipoFilaDinamica;
+typedef struct TipoFila{
+    TipoApontador Frente, Tras;
+}TipoFila;
 
-// Funções de manipulação da Fila Dinamica
+// Fun��es b�sicas de manipula��o da Fila Dinamica
 
-void FFDVazia(TipoFilaDinamica *fila){
-    fila->Frente = (TipoApontadorDinamica) malloc(sizeof(TipoCelula));
+void FFVazia(TipoFila *fila){
+    fila->Frente = (TipoApontador) malloc(sizeof(TipoCelula));
     fila->Tras = fila->Frente;
     fila->Frente->Prox = NULL;
-};
+}
 
-void EnfileiraDinamica(TipoItem x, TipoFilaDinamica *fila){
-    fila->Tras->Prox = (TipoApontadorDinamica) malloc(sizeof(TipoCelula));
+int FilaVazia(TipoFila fila){
+	return (fila.Frente == fila.Tras);
+}
+
+void Enfileira(TipoItem x, TipoFila *fila){
+    fila->Tras->Prox = (TipoApontador) malloc(sizeof(TipoCelula));
     fila->Tras = fila->Tras->Prox;
     fila->Tras->Item = x;
     fila->Tras->Prox = NULL;
-};
+}
 
-void DesenfileiraDinamica(TipoFilaDinamica *fila, TipoItem *item){
-    TipoApontadorDinamica q;
-    if(fila->Frente == fila->Tras){
-        printf("Erro fila esta vazia\n");
+void Desenfileira(TipoFila *fila, TipoItem *item){
+    TipoApontador q;
+    
+    if(FilaVazia(*fila)){
+        printf("Erro: Fila esta vazia\n");
     }
     q = fila->Frente;
     fila->Frente = fila->Frente->Prox;
     *item = fila->Frente->Item;
     free(q);
-};
+}
 
-void ExibeDinamica(TipoFilaDinamica fila){
-    TipoApontadorDinamica aux = fila.Frente->Prox;
+void Exibe(TipoFila fila){
+    TipoApontador aux = fila.Frente->Prox;
+    
     while(aux != NULL){
         printf("\nPlaca==%s | Deslocamento==%d | Prioridade==%d", aux->Item.placa, aux->Item.deslocamento, aux->Item.prioridade);
         aux = aux->Prox;
     }
-};
+}
+
+
+//Fun��es elaboradas para solu��o do probema apresentado
+
+int CalculaQtdItensFila(TipoFila fila){
+	TipoApontador aux;
+	int quantidade = 0;
+	
+	aux = fila.Frente->Prox;
+	while(aux != NULL){
+		quantidade ++;
+	}
+	
+	return quantidade;
+}
+
+void OrganizaFilaEspera(TipoFila *fila){
+	TipoApontador aux;
+	
+	aux = fila->Frente->Prox;
+	while(aux != NULL){
+		
+	}
+}
+
+
 
 int main(){
-
-    TipoFilaEstatica filaEstacionamento;
-    TipoFilaDinamica filaEspera;
-    TipoFilaDinamica filaAuxiliar;
-    FFDVazia(&filaEspera);
-    FFDVazia(&filaAuxiliar);
-    FFEVazia(&filaEstacionamento);
+	//Filas e Itens que auxiliar�o na manipula��o e organiza��o dos estacionamentos
+    TipoFila filaEstacionamento;
+    TipoFila filaEspera;
+    TipoFila filaAuxiliar;
     TipoItem item;
-    int opcao;
+    
+    //Vari�veis auxiliares
+	int opcao, verificador;
+	char resposta[4];
+    
+    
+    //Criando as tr�s filas necess�rias
+    FFVazia(&filaEspera);
+    FFVazia(&filaAuxiliar);
+    FFVazia(&filaEstacionamento);
 
+	//Menu
     do{
         printf("------------------\n");
-        printf("--Escollha uma opção--\n");
-        printf("1.\n");
-        printf("2.\n");
+        printf("\t-- Escolha uma opcao --\n\n");
+        printf("1. Estacionar um Carro\n");
+        printf("2. Retirar um Carro\n");
         printf("0. Sair\n");
         printf("------------------\n");
+        printf("R: ");
         scanf("%d", &opcao);
+        fflush(stdin);
 
         switch(opcao){
             case 1:
-            // Chegada de um carro
-            // Quando um carro chegar, a mensagem devera especificar se existe ou 
-            // nao vaga para o carro dentro do estacionamento. Se nao exister vaga, 
-            // o carro esperara pela vaga na FilaEspera
+	            system("cls");
+				// Chegada de um carro
+	            // Quando um carro chegar, a mensagem devera especificar se existe ou 
+	            // nao vaga para o carro dentro do estacionamento. Se nao exister vaga, 
+	            // o carro esperara pela vaga na FilaEspera
+	            
+	            //Coletando informa��es do carro:
+	            printf("-- Dados do carro --\n\n");
+	            printf(". Informe a placa do carro: ");
+	            gets(item.placa);
+	            
+	            do{
+	            	verificador = 0;
+	            	
+	            	printf(". Voce possui prioridade para estacionar? ");
+	            	gets(resposta);
+	            	
+	            	if(strcmp(resposta, "Sim") == 0 || strcmp(resposta, "sim") == 0){
+		            	item.prioridade = 1;
+		            	verificador =  1;
+					} else if(strcmp(resposta, "Nao") == 0 || strcmp(resposta, "nao") == 0){
+						item.prioridade = 0;
+						verificador = 1;
+					} else{
+						printf("\nR: OPCAO INVALIDA!\n\n");
+						system("pause");
+						printf("\n");
+					}
+				} while(verificador == 0);
+	            
+	            item.deslocamento = 0;
+	            
+	            system("cls");
+	            
+	            /*Verificando se a Tv. Sr. Bom Jesus dos Passos est� com vaga para estacionar. 
+				Se sim, o carro � estacionado nela e, caso contr�rio, ele fica aguardando na 
+				Tv. Monsehor Jo�o Pedro*/
+				if(CalculaQtdItensFila(filaEstacionamento) < 5){
+					Enfileira(item, &filaEstacionamento);
+					printf("\nR: Seu carro esta estacionado na Tv. Sr. Bom Jesus dos Passos.\n\n");
+				} else{
+					Enfileira(item, &filaEspera);
+					printf("\nR: Como a Tv. Sr. Bom Jesus dos Passos esta cheia, seu carro esta aguardando na Tv. Monsenhor Joao Pedro.\n\n");
+				}
+				
+				system("pause");
 
                 break;
             case 2:
-            // Saida de um carro
-            // Quando um carro partir, a mensagem devera incluir o numero de vezes que 
-            // o carro foi deslocado dentro do estacionamento, incluindo a propria partida, 
-            // mas não a chegada
+	                            // Saida de um carro
+                // Quando um carro partir, a mensagem devera incluir o numero de vezes que 
+                // o carro foi deslocado dentro do estacionamento, incluindo a propria partida, 
+                // mas não a chegada
+                printf("-- Saida de veiculo --/n");
+                printf("\nInforme a placa do veiculo: ");
+                gets(item.placa);
+
+                /*
+                  while(não achar o carro)
+				  	verifica se carro está no estacionamento
+					Desinfeleira 
+					deslocamento++
+					carro desenfileirado é o carro que vai sair?
+						printf(informações do carro)
+						print(Relatoria da situacao do estacionamento)
+					Enfileira o carro na fila auxiliar
+					
+                */
+
+			   
+
+                
+                
                 
                 break;
             case 0:
                 printf("Saindo...\n");
                 return 0;
+                
                 break;
             default:
-                printf("Opção inválida\n");
+                printf("Opcao invalida\n");
 
         }
         
 
         system("cls");
-        system("pause");
 
     }while(opcao != 0);
 
