dac0c39f5c08a584f143a75538e00ae03ecf9794
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2024-10-18 11:41:51 +0000

    .

---
+++ 4_Periodo/3_Lista/L03Q03.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void GerarVetorOrdenado(int *vetor, int tam);
+
+void GerarVetorAleatorio(int *vetor, int tam);
+
+int EstaOrdenado(int *vetor, int tam);
+
+void ImprimirVetor(int *vetor, int tam);
+
+
+int main(){
+
+    int i, tam = 10;
+    int vetor[tam];
+
+
+
+
+    GerarVetorAleatorio(vetor, tam); // Gerar um vetor aleatorio no range de 0 a 100;
+    ImprimirVetor(vetor, tam); // Imprimir Vetor
+
+
+    if(EstaOrdenado(vetor, tam)){ // Verifica se o vetor esta ordenado em ordem crescente
+        printf("ORDENADO\n");
+    }else{
+        printf("NAO ESTA ORDENADO\n");
+    }
+
+
+    GerarVetorOrdenado(vetor, tam); // Gerar um vetor ordenado onde cada n é igual a i+i
+
+    ImprimirVetor(vetor, tam); // Imprimir Vetor
+
+    if(EstaOrdenado(vetor, tam)){ // Verifica se o vetor esta ordenado em ordem crescente
+        printf("ORDENADO\n");
+    }else{
+        printf("NAO ESTA ORDENADO\n");
+    }
+
+
+    srand(time(NULL)); // Gerar Semente
+    int n = rand() % 20; // Numero a ser inserido ao vetor
+
+    
+
+
+
+}
+
+
+void GerarVetorOrdenado(int *vetor, int tam){
+    int i;
+    for(i=1; i <tam; i++){
+        vetor[i] = i+i;
+    }
+}
+
+void GerarVetorAleatorio(int *vetor, int tam){
+    srand(time(NULL));
+    int i;
+    for(i = 0; i < tam; i++){
+        vetor[i] = rand()%100;
+    }
+}
+
+int EstaOrdenado(int *vetor, int tam){
+    int i;
+
+    for(i = 0; i < tam; i++){
+        if(vetor[i] > vetor[i+1]){
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void ImprimirVetor(int *vetor, int tam){
+    int i;
+
+    for(i = 0; i< tam; i++){
+        printf("%d ", vetor[i]);
+    }
+}
