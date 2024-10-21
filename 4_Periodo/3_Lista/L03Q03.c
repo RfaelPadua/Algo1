627e52160a240b7a4e297510693f7b7591119b4f
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2024-10-21 20:55:34 -0300

    .

--- 4_Periodo/3_Lista/L03Q03.c
+++ 4_Periodo/3_Lista/L03Q03.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+//Escreva um algoritmo que receba valores em um vetor de 10 posições e 
+//imprima  “ORDENADO” se o vetor estiver em ordem crescente. Qual é a 
+//função de custo de pior caso e sua  ordem de complexidade O?
 
 void GerarVetorOrdenado(int *vetor, int tam);
 
@@ -12,9 +14,10 @@ int EstaOrdenado(int *vetor, int tam);
 void ImprimirVetor(int *vetor, int tam);
 
 
+
 int main(){
 
-    int i, tam = 10;
+    int tam = 10;
     int vetor[tam];
 
 
@@ -54,8 +57,8 @@ int main(){
 
 void GerarVetorOrdenado(int *vetor, int tam){
     int i;
-    for(i=1; i <tam; i++){
-        vetor[i] = i+i;
+    for(i=0; i <tam; i++){
+        vetor[i] = i+i+1;
     }
 }
 
@@ -70,7 +73,7 @@ void GerarVetorAleatorio(int *vetor, int tam){
 int EstaOrdenado(int *vetor, int tam){
     int i;
 
-    for(i = 0; i < tam; i++){
+    for(i = 0; i < tam - 1 ; i++){
         if(vetor[i] > vetor[i+1]){
             return 0;
         }
@@ -82,7 +85,7 @@ int EstaOrdenado(int *vetor, int tam){
 void ImprimirVetor(int *vetor, int tam){
     int i;
 
-    for(i = 0; i< tam; i++){
+    for(i = 0; i < tam; i++){
         printf("%d ", vetor[i]);
     }
 }
