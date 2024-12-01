5a0715ba580a0fac31c690dd6ed49f937fa51ade
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2024-12-01 15:06:17 +0000

    .

--- 4_Periodo/PAA.c
+++ 4_Periodo/PAA.c
@@ -2,6 +2,10 @@
 #include<stdlib.h>
 #include<time.h>
 
+
+
+
+
 #define MAXUNSIGNINT 4294967295
 
 //função que volta todos os numeros primos até um numero n
@@ -13,7 +17,7 @@ int numerosPrimosForcaBruta(int n, int *primos){
                 break;
             }
         }
-        if(j == i){
+        if(j == i){ 
             primos[cont] = i;
             cont++;
         }
@@ -41,29 +45,80 @@ int numerosPrimosCrivo(int n, int *primos){
     return cont;
 }
 
-int main(){
-    int n = 1000000;
-    int *primos = (int*) malloc((n+1) * sizeof(int));
+
+#ifdef _WIN32
+#include <windows.h>
+#include <time.h>
+
+void medirDesempenho(int n, int *primos){
     clock_t inicio, fim;
-    double tempo;
 
     inicio = clock();
-    int qtdPrimos = numerosPrimosCrivo(n, primos);
+    int numerosPrimos = numerosPrimosForcaBruta(n, primos);
     fim = clock();
-    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("Crivo\n");
-    printf("Quantidade de primos: %d\n", qtdPrimos);
-    printf("Tempo: %lf\n", tempo);
-    printf("\n");
-
-    inicio = clock();
-    qtdPrimos = numerosPrimosForcaBruta(n, primos);
-    fim = clock();
-    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
     printf("Forca Bruta\n");
-    printf("Quantidade de primos: %d\n", qtdPrimos);
-    printf("Tempo: %lf\n", tempo);
+    printf("Tempo: %lf\n", (double)(fim - inicio) / CLOCKS_PER_SEC);
+    printf("Quantidade de primos: %d\n", numerosPrimos);
     printf("\n");
+
+    inicio = clock();
+    numerosPrimos = numerosPrimosCrivo(n, primos);
+    fim = clock();
+    printf("Crivo\n");
+    printf("Tempo: %lf\n", (double)(fim - inicio) / CLOCKS_PER_SEC);
+    printf("Quantidade de primos: %d\n", numerosPrimos);
+    printf("\n");
+
+
+}
+
+
+#else
+
+#include <sys/resource.h>
+
+void medirDesempenho(int n, int *primos){
+    struct rusage usage;
+    struct timeval start, end;
+    double tempo;
+    getrusage(RUSAGE_SELF, &usage);
+    start = usage.ru_utime;
+    int numerosPrimos = numerosPrimosForcaBruta(n, primos);
+    getrusage(RUSAGE_SELF, &usage);
+    end = usage.ru_utime;
+    tempo = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+    printf("Forca Bruta\n");
+    printf("Tempo: %lf\n", tempo);
+    printf("Quantidade de primos: %d\n", numerosPrimos);
+    printf("\n");
+
+    getrusage(RUSAGE_SELF, &usage);
+    start = usage.ru_utime;
+    numerosPrimos = numerosPrimosCrivo(n, primos);
+    getrusage(RUSAGE_SELF, &usage);
+    end = usage.ru_utime;
+    tempo = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+    printf("Crivo\n");
+    printf("Tempo: %lf\n", tempo);
+    printf("Quantidade de primos: %d\n", numerosPrimos);
+    printf("\n");
+
+}
+
+
+#endif
+
+int main(){
+    int n = 10;
+    
+
+    for(;n <= 1000000000; n *= 10){
+        int *primos = (int*) malloc((n+1) * sizeof(int));
+        printf("N: %d\n", n);
+        medirDesempenho(n, primos);
+        printf("\n");
+        free(primos);
+    }
 
 
     return 0;
